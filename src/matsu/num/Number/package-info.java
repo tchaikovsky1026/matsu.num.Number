01d4b95447e6c73277e0b9117cd44d547bf2b2b5
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-07-28 18:03:33 +0900

    add: モジュール定義とモジュール構成に関わるファイル

---
+++ src/matsu/num/Number/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * 
+ */
+package matsu.num.Number;
---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * 
+ */
+module matsu.num.Number {
+}

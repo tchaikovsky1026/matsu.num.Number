7cf0bcb76a1cb5724d181c64279cdd883980d683
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-08-04 12:28:19 +0900

    docs: モジュール定義,READMEの更新

--- src/module-info.java
+++ src/module-info.java
@@ -6,7 +6,21 @@
  */
 
 /**
+ * 整数に関する数値計算を扱うモジュール.
  * 
+ * <p>
+ * このモジュールでは, 整数に関する数値計算機能を提供する. <br>
+ * また, {@code int} や {@code long} 等の整数型に関する機能を提供する.
+ * </p>
+ * 
+ * <p>
+ * <i>依存モジュール:</i> <br>
+ * (無し)
+ * </p>
+ * 
+ * @author Matsuura Y.
+ * @version 0.1.0
  */
 module matsu.num.Number {
+    exports matsu.num.number;
 }

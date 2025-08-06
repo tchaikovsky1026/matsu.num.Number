9d6206ce0c610dc48e96aaccb263052435118a51
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-08-06 17:02:42 +0900

    feat,test: 2^s*m型の除数を法とするModulusIntのプロトタイプ

---
+++ test/matsu/num/number/incubator/modulo/MontgomeryBasedModulusIntTest.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+package matsu.num.number.incubator.modulo;
+
+import java.util.function.IntFunction;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import matsu.num.number.speedutil.SpeedTestExecutor;
+
+/**
+ * {@link MontgomeryBasedModulusInt} クラスのテスト.
+ */
+@RunWith(Enclosed.class)
+final class MontgomeryBasedModulusIntTest {
+
+    public static final Class<?> TEST_CLASS = MontgomeryBasedModulusInt.class;
+
+    private static final IntFunction<ModulusInt> moduloGetter =
+            m -> {
+                int exponent = Integer.numberOfTrailingZeros(m);
+                int innerDivisor = m >> exponent;
+
+                if (!(exponent >= 1 && innerDivisor != 1)) {
+                    throw new UnsupportedOperationException();
+                }
+
+                return new MontgomeryBasedModulusInt(m);
+            };
+
+    public static class ModProd2のテスト extends ModulusIntTesting.Prod2 {
+
+        @Override
+        ModulusInt getModulusInt(int m) {
+            return moduloGetter.apply(m);
+        }
+    }
+
+    public static class ModProdArrayのテスト extends ModulusIntTesting.ProdArray {
+
+        @Override
+        ModulusInt getModulusInt(int m) {
+            return moduloGetter.apply(m);
+        }
+    }
+
+    public static class ModPowのテスト extends ModulusIntTesting.Pow {
+
+        @Override
+        ModulusInt getModulusInt(int m) {
+            return moduloGetter.apply(m);
+        }
+    }
+
+    @Ignore
+    public static class 計算時間評価 {
+
+        private int m = 31 * 64;
+        private int k = 100;
+
+        private int d = 100000;
+
+        @Test
+        public void test_MontgomeryBasedModulusIntの実行() {
+            ModulusInt modulusInt = new MontgomeryBasedModulusInt(m);
+            {
+                var executor = new SpeedTestExecutor(
+                        TEST_CLASS, "MontgomeryBasedModulusInt:mod: ", 10_000_000,
+                        () -> {
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                        });
+                executor.execute();
+            }
+            {
+                var executor = new SpeedTestExecutor(
+                        TEST_CLASS, "MontgomeryBasedModulusInt:modpr2: ", 10_000_000,
+                        () -> {
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                        });
+                executor.execute();
+            }
+            {
+                var executor = new SpeedTestExecutor(
+                        TEST_CLASS, "MontgomeryBasedModulusInt:modpow: ", 2_000_000,
+                        () -> {
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                        });
+                executor.execute();
+            }
+        }
+
+        @Test
+        public void test_SimpleModulusIntの実行() {
+            ModulusInt modulusInt = SimpleModulusInt.of(m);
+            {
+                var executor = new SpeedTestExecutor(
+                        TEST_CLASS, "SimpleModulusInt:mod: ", 10_000_000,
+                        () -> {
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                            d += modulusInt.mod(d);
+                        });
+                executor.execute();
+            }
+            {
+                var executor = new SpeedTestExecutor(
+                        TEST_CLASS, "SimpleModulusInt:modpr2: ", 10_000_000,
+                        () -> {
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                            d += modulusInt.modpr(d, d);
+                        });
+                executor.execute();
+            }
+            {
+                var executor = new SpeedTestExecutor(
+                        TEST_CLASS, "SimpleModulusInt:modpow: ", 2_000_000,
+                        () -> {
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                            d += modulusInt.modpow(d, k);
+                        });
+                executor.execute();
+            }
+        }
+    }
+}

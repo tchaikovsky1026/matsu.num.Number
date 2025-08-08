0524d026b5788889e6974f4af845d93e42130acc
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-08-08 20:45:01 +0900

    feat,test: ModulusLongPow2の実装とテスト,Mod1Longの実装

---
+++ src/matsu/num/number/modulo/Mod1Long.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/*
+ * 2025.8.8
+ */
+package matsu.num.number.modulo;
+
+import java.util.Objects;
+
+/**
+ * 除数1に対する {@link ModulusLong} の実装.
+ * 
+ * @author Matsuura Y.
+ */
+final class Mod1Long implements ModulusLong {
+
+    /**
+     * シングルトン.
+     */
+    static final ModulusLong INSTANCE = new Mod1Long();
+
+    /**
+     * 内部から呼ばれる.
+     */
+    private Mod1Long() {
+        super();
+    }
+
+    @Override
+    public long divisor() {
+        return 1L;
+    }
+
+    @Override
+    public long mod(long x) {
+        return 0L;
+    }
+
+    @Override
+    public long modpr(long x, long y) {
+        return 0L;
+    }
+
+    @Override
+    public long modpr(long... x) {
+        Objects.requireNonNull(x);
+        return 0L;
+    }
+
+    @Override
+    public long modpow(long x, int k) {
+        if (k < 0L) {
+            throw new IllegalArgumentException("illegal: exponent k is negative: k = " + k);
+        }
+        return 0L;
+    }
+}
---
+++ src/matsu/num/number/modulo/ModulusLongPow2.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/*
+ * 2025.8.8
+ */
+package matsu.num.number.modulo;
+
+/**
+ * 2の累乗を除数とする, {@link ModulusLong}.
+ * 
+ * @author Matsuura Y.
+ */
+final class ModulusLongPow2 implements ModulusLong {
+
+    private final long divisor;
+    private final long bitMask;
+
+    /**
+     * 指数 k を与えて, 2^k を法としたモジュロ演算を構築する.
+     * 
+     * <p>
+     * {@literal 1 <= k <= 62} でなければならない. <br>
+     * 引数のバリデーションは行われていないので,
+     * 呼び出しもとでチェックすること.
+     * </p>
+     * 
+     * @param exponent 指数
+     */
+    ModulusLongPow2(int exponent) {
+        super();
+        assert 1 <= exponent && exponent <= 62;
+
+        this.divisor = 1L << exponent;
+        this.bitMask = this.divisor - 1;
+    }
+
+    @Override
+    public long divisor() {
+        return this.divisor;
+    }
+
+    @Override
+    public long mod(long x) {
+        return x & bitMask;
+    }
+
+    @Override
+    public long modpr(long x, long y) {
+        // 注: 2の累乗を法としたmodは, 符号ありなしで同一である.
+        return (x * y) & bitMask;
+    }
+
+    @Override
+    public long modpr(long... x) {
+        // 注: 2の累乗を法としたmodは, 符号ありなしで同一である.
+
+        switch (x.length) {
+            case 0:
+                return 1L;
+            case 1:
+                return mod(x[0]);
+            case 2:
+                return modpr(x[0], x[1]);
+            default:
+                // ブロック外で処理
+        }
+
+        // サイズ3以上
+        x = x.clone();
+        int len = x.length;
+
+        // 結合法則を利用して, 4系列に分割
+        // mcの単位元で初期化
+        long v0 = 1;
+        long v1 = 1;
+        long v2 = 1;
+        long v3 = 1;
+        int i;
+        for (i = 0; i < len - 3; i += 4) {
+            v0 *= x[i];
+            v1 *= x[i + 1];
+            v2 *= x[i + 2];
+            v3 *= x[i + 3];
+        }
+        for (; i < len; i++) {
+            v0 *= x[i];
+        }
+
+        return ((v0 * v1) * (v2 * v3)) & bitMask;
+    }
+
+    @Override
+    public long modpow(long x, int k) {
+        if (k < 0) {
+            throw new IllegalArgumentException("illegal: exponent k is negative: k = " + k);
+        }
+
+        switch (k) {
+            case 0:
+                return 1L;
+            case 1:
+                return mod(x);
+            case 2:
+                return modpr(x, x);
+            default:
+                // ブロック外で処理
+        }
+
+        // 指数3以上
+        long out = 1L;
+        long xPow = x;
+        while (k > 0) {
+            if ((k & 1) == 1) {
+                out *= xPow;
+            }
+
+            k >>= 1;
+            xPow *= xPow;
+        }
+
+        return out & bitMask;
+    }
+}

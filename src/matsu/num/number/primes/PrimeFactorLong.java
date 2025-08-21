d6e5898b6f3893f59779761142061021f83d7551
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-08-22 08:29:07 +0900

    docs: Javadocの更新

--- src/matsu/num/number/primes/PrimeFactorLong.java
+++ src/matsu/num/number/primes/PrimeFactorLong.java
@@ -275,11 +275,13 @@ public final class PrimeFactorLong implements Comparable<PrimeFactorLong> {
     }
 
     /**
-     * {@code this} から素因数をひとつだけ取り除いた
+     * {@code this} から素因数をひとつだけ取り除いた ({@link #dividedBy(long)} を適用した)
      * {@link PrimeFactorLong} の重複なしのバリエーションを列挙するイミュータブルコレクションを返す.
      * 
      * <p>
-     * 返される {@link Collection} は拡張 {@code for} 文や {@link Stream}
+     * 返される {@link Collection} は,
+     * 要素を取得するために毎回 {@link #dividedBy(long)} を実行する可能性がある. <br>
+     * そのため, 拡張 {@code for} 文や {@link Stream}
      * を使うことを補助する目的に適している. <br>
      * コレクションを実体として取り扱う場合,
      * 他の {@link Collection} の実装に詰め直したほうが良い.
@@ -290,6 +292,7 @@ public final class PrimeFactorLong implements Comparable<PrimeFactorLong> {
      * </p>
      * 
      * @return 素因数をひとつだけ取り除いた素因数分解のコレクション
+     * @see #dividedBy(long)
      */
     public final Collection<PrimeFactorLong> subFactorsCollection() {
 

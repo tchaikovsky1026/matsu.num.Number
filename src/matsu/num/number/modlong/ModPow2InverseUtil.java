f0901f6c54167b2f3b95fdf03d10871262e4ce0a
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-07-05 15:20:51 +0900

    fix: ModPow2InverseUtilのリファクタとコメント追加

--- src/matsu/num/number/modlong/ModPow2InverseUtil.java
+++ src/matsu/num/number/modlong/ModPow2InverseUtil.java
@@ -6,13 +6,20 @@
  */
 
 /*
- * 2025.8.6
+ * 2026.7.5
  */
 package matsu.num.number.modlong;
 
 /**
- * 整数の, 2^(bit) に対するモジュロ逆元を計算することに関するプロトタイプ. <br>
- * 2^(32), 2^(64) に対応し, 符号なし整数として扱う.
+ * 64-bit 整数の 2^64 に対するモジュロ逆元を計算するユーティリティ. <br>
+ * 符号有り/無しを問わない.
+ * 
+ * <p>
+ * 機能メモ: <br>
+ * m に対し, ym = 1 mod 2^64 とする. <br>
+ * このとき, 1 以上 64 以下の d について, ym = 1 mod 2^d である. <br>
+ * すなわちこの機能は, 2^d を法とした逆元の計算に使用できる.
+ * </p>
  * 
  * @author Matsuura Y.
  */
@@ -24,37 +31,56 @@ final class ModPow2InverseUtil {
     }
 
     /**
-     * 符号なし long について, 2^(64) を法とした逆元を計算する.
+     * {@code long} 型整数について, R = 2^64 を法とした逆元を計算する.
      * 
      * <p>
-     * n は奇数でなければならない.
+     * n は奇数でなければならない. <br>
+     * ただし, このバリデーションは行われない.
      * </p>
      * 
-     * @param n 符号なし整数
-     * @return n^(-1) (mod 2^(64))
+     * @param n n
+     * @return R = 2^64 を法とした n の逆元
      */
     static long invModR(long n) {
         assert (n & 1L) == 1L;
 
-        long r = 1L;
-        long currentRMask = -1L;
-        long currentBitSetMask = 1L;
-        long out = 0;
-        while (r != 0) {
-            if ((r & 1L) == 1L) {
-                // outの注目ビットを立てる
-                out |= currentBitSetMask;
+        /*
+         * (n は奇数であることに注意.)
+         * r = 1 - y*n を表し, r = 0 となるように y を決定する.
+         * 初期値は y = 0 (よって r = 1) とする.
+         * 
+         * y は下位 bit から順番に決定していく.
+         * y の下位 a-bit を決定しようとしているとき (a >= 0), 上記 r は (a-1)-bit 以下はすべて 0
+         * で埋まっている.
+         * そこで, y の下位 a-bit を決定しようとしているときは,
+         * r としては (a-1)-bit 以下があふれるように r >>> a を考えればよい.
+         */
 
-                r -= n;
+        long y = 0;
+        // r >>> a の値を表す.
+        long currentR = 1L;
+        // r >>> a の真の値を表現するためのマスク, 処理が進むと上位bitが 0 で埋まる
+        long currentRMask = -1L;
+        // 現在処理している y　の桁の bit が立っている
+        long currentBitSetMask = 1L;
+        while (currentR != 0) {
+            if ((currentR & 1L) == 1L) {
+                // y の注目ビットを立てる
+                y |= currentBitSetMask;
+
+                currentR -= n;
+                // この時点では, currentR の上位 bit にゴミが入っている.
             }
 
-            r &= currentRMask;
-            r >>>= 1;
-            currentRMask >>>= 1;
+            //currentR の上位 bit のゴミを消去することで, 真の (r >>> a) の値に変換
+            currentR &= currentRMask;
 
+            // y の注目 bit を1つ上げる
+            currentR >>>= 1;
+            currentRMask >>>= 1;
             currentBitSetMask <<= 1;
         }
 
-        return out;
+        return y;
     }
 }

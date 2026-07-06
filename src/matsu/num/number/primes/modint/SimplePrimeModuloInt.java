41f4452f2e2f0d3feb46793e2d96a5aa1ac125d5
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-07-06 14:13:10 +0900

    docs: SimplePrimeModuloInt,SimplePrimeModuloLongに位数と原始根に関するコメントを追加

--- src/matsu/num/number/primes/modint/SimplePrimeModuloInt.java
+++ src/matsu/num/number/primes/modint/SimplePrimeModuloInt.java
@@ -23,8 +23,31 @@ import matsu.num.number.primes.PrimeModuloInt;
 final class SimplePrimeModuloInt extends SkeletalPrimeModuloInt {
 
     /*
-     * 素数を法とするモジュロ演算における, 位数と原始根について
+     * 素数を法とするモジュロ演算における, 位数と原始根に関する.
+     * 除数を p (素数) とする.
      * 
+     * 1 <= a < p である a について,
+     * a の位数とは, a^m = 1 (mod p) であるような最小の正整数 m である.
+     * 位数の性質として次が成り立つ.
+     * 
+     * m は (p-1) の約数である.
+     * a^q = 1 (mod p) を満たすすべての q は m の倍数である.
+     * 
+     * 位数計算アルゴリズム:
+     * 1. a^q = 1 (mod p) なる q がわかっているとする.
+     * 2. q の素因数の集合 {p_1, p_2, ...} を考え, q_k = q/p_k とし, a^(q_k) mod p を計算する.
+     * 3. a^(q_k) = 1 (mod p) なる q_k があった場合, q := q_k として 2 にもどる.
+     * (一度見つかればそれ以降の k を検証する必要はない).
+     * ない場合は m = q である.
+     * 
+     * 初期値は q = p-1 とする.
+     * 
+     * 
+     * 1 <= a < p である a について,
+     * a が p を法としての原始根であるとは, a の位数が p-1 であることである.
+     * a が原始根であるかを判定するには,
+     * q の素因数の集合 {p_1, p_2, ...} を考え, q_k = q/p_k とし,
+     * すべての k について a^(q_k) != 1 (mod p) を確かめればよい.
      */
 
     /**
@@ -63,6 +86,14 @@ final class SimplePrimeModuloInt extends SkeletalPrimeModuloInt {
     int orderConcrete(int a) {
         PrimeFactorInt orderCandidate = factorOfPm1;
 
+        /*
+         * 位数は a^q = 1 (mod p) なる q の約数であることを使う.
+         * 
+         * q の素因数の集合 {p_1, p_2, ...} を考え, q_k = q/p_k とし,
+         * a^(q_k) mod p を計算する.
+         * 
+         * それらが 1 に一致しなければ q は位数であり, 一致すれば q := q_k としてさらに約数を考える.
+         */
         candidateValidation: while (true) {
             for (PrimeFactorInt sub : orderCandidate.subFactorsCollection()) {
                 if (this.modpow(a, sub.original()) == 1) {
@@ -71,7 +102,7 @@ final class SimplePrimeModuloInt extends SkeletalPrimeModuloInt {
                 }
             }
 
-            // sub.ogininal の mod p がどれも1でないなら, それは位数である
+            // sub.original の mod p がどれも1でないなら, それは位数である
             break;
         }
 
@@ -81,6 +112,10 @@ final class SimplePrimeModuloInt extends SkeletalPrimeModuloInt {
     @Override
     boolean isPrimitiveRootConcrete(int a) {
 
+        /*
+         * q=p-1 の素因数の集合 {p_1, p_2, ...} を考え, q_k = q/p_k とし,
+         * すべての k について a^(q_k) != 1 mod p かどうかを確かめる.
+         */
         for (PrimeFactorInt sub : factorOfPm1.subFactorsCollection()) {
             if (this.modpow(a, sub.original()) == 1) {
                 return false;
--- src/matsu/num/number/primes/modlong/SimplePrimeModuloLong.java
+++ src/matsu/num/number/primes/modlong/SimplePrimeModuloLong.java
@@ -22,6 +22,34 @@ import matsu.num.number.primes.PrimeModuloLong;
  */
 final class SimplePrimeModuloLong extends SkeletalPrimeModuloLong {
 
+    /*
+     * 素数を法とするモジュロ演算における, 位数と原始根に関する.
+     * 除数を p (素数) とする.
+     * 
+     * 1 <= a < p である a について,
+     * a の位数とは, a^m = 1 (mod p) であるような最小の正整数 m である.
+     * 位数の性質として次が成り立つ.
+     * 
+     * m は (p-1) の約数である.
+     * a^q = 1 (mod p) を満たすすべての q は m の倍数である.
+     * 
+     * 位数計算アルゴリズム:
+     * 1. a^q = 1 (mod p) なる q がわかっているとする.
+     * 2. q の素因数の集合 {p_1, p_2, ...} を考え, q_k = q/p_k とし, a^(q_k) mod p を計算する.
+     * 3. a^(q_k) = 1 (mod p) なる q_k があった場合, q := q_k として 2 にもどる.
+     * (一度見つかればそれ以降の k を検証する必要はない).
+     * ない場合は m = q である.
+     * 
+     * 初期値は q = p-1 とする.
+     * 
+     * 
+     * 1 <= a < p である a について,
+     * a が p を法としての原始根であるとは, a の位数が p-1 であることである.
+     * a が原始根であるかを判定するには,
+     * q の素因数の集合 {p_1, p_2, ...} を考え, q_k = q/p_k とし,
+     * すべての k について a^(q_k) != 1 (mod p) を確かめればよい.
+     */
+
     /**
      * p-1の素因数分解を扱う.
      */
@@ -58,6 +86,14 @@ final class SimplePrimeModuloLong extends SkeletalPrimeModuloLong {
     long orderConcrete(long a) {
         PrimeFactorLong orderCandidate = factorOfPm1;
 
+        /*
+         * 位数は a^q = 1 (mod p) なる q の約数であることを使う.
+         * 
+         * q の素因数の集合 {p_1, p_2, ...} を考え, q_k = q/p_k とし,
+         * a^(q_k) mod p を計算する.
+         * 
+         * それらが 1 に一致しなければ q は位数であり, 一致すれば q := q_k としてさらに約数を考える.
+         */
         candidateValidation: while (true) {
             for (PrimeFactorLong sub : orderCandidate.subFactorsCollection()) {
                 if (this.modpow(a, sub.original()) == 1) {
@@ -66,7 +102,7 @@ final class SimplePrimeModuloLong extends SkeletalPrimeModuloLong {
                 }
             }
 
-            // sub.ogininal の mod p がどれも1でないなら, それは位数である
+            // sub.original の mod p がどれも1でないなら, それは位数である
             break;
         }
 
@@ -76,6 +112,10 @@ final class SimplePrimeModuloLong extends SkeletalPrimeModuloLong {
     @Override
     boolean isPrimitiveRootConcrete(long a) {
 
+        /*
+         * q=p-1 の素因数の集合 {p_1, p_2, ...} を考え, q_k = q/p_k とし,
+         * すべての k について a^(q_k) != 1 mod p かどうかを確かめる.
+         */
         for (PrimeFactorLong sub : factorOfPm1.subFactorsCollection()) {
             if (this.modpow(a, sub.original()) == 1) {
                 return false;
